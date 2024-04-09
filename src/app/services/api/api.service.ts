5438680196cf4960bcfc9a948e5290eefdf4852d
renhubgit <renik2001@gmail.com>
2024-04-09 22:52:54 +0200

    deleted unused files

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpOptions } from '../../types/http.types';
+import { HttpOptions } from '../../../types/http.types';
 
 @Injectable({
   providedIn: 'root',
