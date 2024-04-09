7e2f565615d81b1dea6138348569501f5ff39dd3
renhubgit <renik2001@gmail.com>
2024-04-09 22:51:17 +0200

    added theme setter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { ThemeService } from './services/theme/theme.service';
+import { LocalStorageKey } from '../enums/local-storage-key.enum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ecommerce-angular';
-
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private themeService: ThemeService) {
+    // set theme (light theme is default)
+    themeService.setTheme(localStorage.getItem(LocalStorageKey.Theme) === 'dark' ? 'dark' : 'light');
     // set default language
     translate.setDefaultLang('en');
   }
