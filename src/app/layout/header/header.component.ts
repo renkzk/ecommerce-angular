2f7853066b8492f503e3cb6b24e23e7a7286af37
renhubgit <renik2001@gmail.com>
2024-04-09 22:52:39 +0200

    added theme-switcher to header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { ThemeSwitcherComponent } from '../../components/theme/theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ThemeSwitcherComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
